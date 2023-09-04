6187a12006c91d325584eb1f6e312d5e78fc0954
Ramiz Hida <ramizhida00@gmail.com>
2023-09-04 03:13:16 +0200

    connect database to API on Heroku

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlix', {
+// mongoose.connect('mongodb://localhost:27017/myFlix', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
