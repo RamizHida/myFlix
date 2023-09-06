801696f0302f02f2f3161317f10a3f845c49e772
Ramiz Hida <ramizhida00@gmail.com>
2023-09-06 15:25:44 +0200

    troubleshoot

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.post(
     }
 
     let hashedPassword = Users.hashPassword(req.body.password);
-    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    await Users.findOne({ userName: req.body.userName }) // Search to see if a user with the requested username already exists
       .then((user) => {
         if (user) {
           //If the user is found, send a response that it already exists
