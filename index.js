dd30706e939401a1296dbc55bfb215b014df2d6d
Ramiz Hida <ramizhida00@gmail.com>
2023-09-06 15:18:54 +0200

    troubleshoot

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ const Users = Models.User;
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect(process.env.CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://myFlixDBAdmin:RamizRocks@myflixdb.javx9xm.mongodb.net/myFlixDB?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const express = require('express');
 const morgan = require('morgan');
