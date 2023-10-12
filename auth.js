2ab8ea4b22bcb2f78069505e35c89c5eb6a121a2
Ramiz Hida <ramizhida00@gmail.com>
2023-10-13 01:46:39 +0200

    update auth for login

--- auth.js
+++ auth.js
@@ -17,7 +17,7 @@ const generateJWTToken = (user) => {
 module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
+      if (error) {
         return res.status(400).json({
           message: 'Something is not right',
           user: user,
